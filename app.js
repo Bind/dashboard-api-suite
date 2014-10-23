d296b72ed475ddbcaf1dd437c1b14ec4bbafa6aa
Doug-Binder <binderd@stolaf.edu>
2014-10-23 15:38:12 -0400

    process port

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ try {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.set('port', process.env.PORT || 3000);
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -114,4 +114,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(8000);
+app.listen(app.get('port'));
+g
