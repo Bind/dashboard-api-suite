54b023321c0f2766b465cb073f6cde6e3a157c60
Doug-Binder <binder@Douglass-MacBook-Air.local>
2014-10-28 20:45:29 -0400

    chart working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,11 @@
 
 
 var init = function(){
-    console.log(topArticles( MostRecentCampaign(CAMPAIGNS).Clicks))
-    console.log(topOpeners(MostRecentCampaign(CAMPAIGNS).Opens))
+
     Openers(topOpeners(MostRecentCampaign(CAMPAIGNS).Opens))
     Clickers( topArticles( MostRecentCampaign(CAMPAIGNS).Clicks))
-    console.log(opensOverTime(MostRecentCampaign(CAMPAIGNS).Clicks))
+    dash(clicksOverTime(MostRecentCampaign(CAMPAIGNS).Clicks))
+    
 }
 
 
@@ -55,18 +55,33 @@ var topOpeners = function(Opens){
 
 }
 
-var opensOverTime = function(Opens){
+var clicksOverTime = function(Opens){
     var clicks = []
         Opens.forEach(function(curr){
-        console.log(curr);
                 curr.raw.forEach(function(el2){
-                    clicks.push({action: 'click', timestamp: curr.timestamp, user: curr.user})
+                    clicks.push({action: 'click', timestamp: clipTimeStamp(el2.timestamp), user: curr.user})
                 })
         })
-    return clicks;
+        var _clicks = clicks.reduce(function(prev, curr, ind, arr){
+                if ( typeof prev[curr.timestamp] == 'undefined'){
+                    prev[curr.timestamp]=1;
+                        } else prev[curr.timestamp]++
+                return prev
+        }, {})
+        var data = [];
+        var keys = Object.keys(_clicks);
+        for (key in keys){
+            data.push({date:keys[key], clicks:_clicks[keys[key]]})
+        }
+        return data;
 }
 
-
+var clipTimeStamp = function(_time){
+        var format = d3.time.format("%Y-%m-%d %X")
+        var current = format.parse(_time).getTime()
+        current -= current % (43200000/2);
+        return format(new Date(current))
+}
 
 
 
@@ -75,79 +90,75 @@ var opensOverTime = function(Opens){
 
 
 
-function dash (){
+var dash = function(data){
 
-var width = 420,
-    barHeight = 20;
+data.sort(function(prev, curr){
+        var format = d3.time.format("%Y-%m-%d %X")
+        var current = format.parse(curr.date).getTime()
+        var previous = format.parse(prev.date).getTime()
+        return previous - current;
+    })
 
-var max = CAMPAIGNS.reduce(function(prev, v){
-    var curr = v['unique_opens']
-        if (prev < curr) return curr;
-        else return prev
-},0)
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 610 - margin.left - margin.right,
+    height = 320 - margin.top - margin.bottom;
 
-for (var key in CAMPAIGNS){
-var campaign = CAMPAIGNS[key];
-var labels = ['unique_clicks',
-		 'unique_opens',
-		 'clicks'];
+var parseDate = d3.time.format("%Y-%m-%d %X").parse;
 
-
-//graph.style('background-color' ,'black')
-var data = grab(campaign, labels)
-
-var x = d3.scale.linear()
-    .domain([0, max])
+var x = d3.time.scale()
     .range([0, width]);
 
-var chart = d3.selectAll(".camp-"+ campaign.id )
-				.attr("width",200+width)
-    			.attr("height", barHeight * data.length);;
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var line = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.clicks); });
+
+var svg = d3.select(".Rates").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-var bar = chart.selectAll("g")
-    .data(data)
-  	.enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
-
-bar.append("text")
-    .attr("x", function(d) { return x(d -2); })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .style('text-anchor', 'end')
-    .text(function(d) { return d; });
+  data.forEach(function(d) {
+    d.date = parseDate(d.date);
+    
+    })
 
 
-bar.append('text')
-    .attr('x', function(d) {return x(d + 2); })
-    .attr('y', barHeight / 2)
-    .attr('dy', ".35em")
-    .style('text-anchor', 'start')
-    .text(function(d, i) {return mapLabel(labels[i])})
-    console.log(campaign)
-    }
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.clicks; }));
 
-}
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
-var grab = function(from, labels){
-//from is an object, labels is an array of attributes you want returned
-var data = [];
-for (var i in labels){
-	var key = labels[i];
-	data.push(from[key]);
-}
-return data;
-}
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
 
-var mapLabel = function(label){
-    var dict = {
-        'unique_clicks':'Users who clicked through',
-        'unique_opens':'Users who opened',
-        'clicks':'Total Clicks',
-    }
-    return dict[label];
-}
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Price ($)");
+
+  svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", line);
+    
+};
+
